20b6444652e9df6a3eb51a8a82755558b1686865
DeinerSierra <deiner.sierra@gmail.com>
2023-04-23 10:46:28 -0500

    first commit

---
+++ client/build/precache-manifest.0830f2c904fefe3e9e9ca447bd67153e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9f4469b1947b7b41ac8273b2bcda99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6c72cabd92a88df35b",
+    "url": "/static/css/main.c1862323.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6c72cabd92a88df35b",
+    "url": "/static/js/main.5f837a55.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
